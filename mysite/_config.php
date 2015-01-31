56a5ec9d2708969044ab1e01fd20f7c873595fa1
Merlin Steuer <2steuer@informatik.uni-hamburg.de>
2015-01-31 23:56:34 +0100

    adding environment configuration

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,23 +1,18 @@
 <?php
 
-Director::set_environment_type('dev');
-error_reporting(E_ALL);
+
 SS_Log::add_writer(new SS_LogFileWriter('sserrors.txt'), SS_Log::NOTICE, '<=');
 
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'root',
-	"password" => '',
-	"database" => 'prueftermine',
-	"path" => '',
-);
+global $database;
+$database = 'prueftermine';
 
 // Set the site locale
 i18n::set_locale('de_DE');
 LocalDateHelper::setLocale('de_DE');
 date_default_timezone_set('Europe/Berlin');
+
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
